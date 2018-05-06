59862dc73dd868277a8374c82b326949dab0e7b6
Andrieu Quentin <quentinandrieumail@gmail.com>
2018-05-06 19:10:22 +1200

    Update desigm

--- src/containers/global/app.component.js
+++ src/containers/global/app.component.js
@@ -52,10 +52,10 @@ class App extends Component {
         <div>
           <div className="section">
             <Row>
-              <Col className="side-nav-menu" s={12} m={12} l={4}>
+              <Col className="side-nav-menu" s={12} m={12} l={3}>
                 <SideNav />
               </Col>
-              <Col s={12} m={12} l={8}>
+              <Col s={12} m={12} l={9}>
                 <Root />
               </Col>
             </Row>
--- src/containers/global/side-nav.component.js
+++ src/containers/global/side-nav.component.js
@@ -16,39 +16,62 @@ class SideNav extends Component {
     }
 
     isActive(link) {
-        return this.props.title === link ? 'active' : '';
+        return this.props.title === link ? 'active' : 'toto';
     }
 
     render() {
-
         return (
             <div>
                 <ul className="collection side-nav-custom hide-on-med-and-down">
-                    <Link className={'collection-item ' + this.isActive('home')} to="/">Home</Link>
-                    <Link className={'collection-item ' + this.isActive('list-patient')} to="/patients">List patient</Link>
-                    <Link className={'collection-item ' + this.isActive('create-patient')} to="/patient/create">Create patient</Link>
-                    <Link className={'collection-item ' + this.isActive('profile')} to="/profile">Profile</Link>
-                    <Link className={'collection-item ' + this.isActive('calendar')} to="/calendar">Calendar</Link>
-                    <Link className="collection-item" onClick={this.logOut} to="/signin">Sign out</Link>
+                    <Link className="collection-item" to="/">
+                        <i className={'small material-icons icon-sidenav ' + this.isActive('home')}>home</i> Home
+                    </Link>
+                    <Link className="collection-item" to="/patients">
+                        <i className={'small material-icons icon-sidenav ' + this.isActive('list-patient')}>list</i> List patient
+                    </Link>
+                    <Link className="collection-item" to="/patient/create">
+                        <i className={'small material-icons icon-sidenav ' + this.isActive('create-patient')}>create</i> Create patient
+                    </Link>
+                    <Link className="collection-item" to="/calendar">
+                        <i className={'small material-icons icon-sidenav ' + this.isActive('calendar')}>perm_contact_calendar</i> Calendar
+                    </Link>
+                    <Link className="collection-item" to="/profile">
+                        <i className={'small material-icons icon-sidenav ' + this.isActive('profile')}>person</i> Profile
+                    </Link>
+                    <Link className="collection-item" onClick={this.logOut} to="/signin">
+                        <i className="small material-icons icon-sidenav">arrow_drop_down_circle</i> Sign out
+                    </Link>
                 </ul>
                 <Row className="hide-on-large-only">
                     <Col s={2}>
-                        <Link to="/"><i class="small material-icons">home</i></Link>
+                        <Link className="link-nav" to="/">
+                            <i className={'small material-icons icon-sidenav ' + this.isActive('home')}>home</i>
+                        </Link>
                     </Col>
                     <Col s={2}>
-                        <Link to="/patients"><i class="small material-icons">list</i></Link>
+                        <Link className="link-nav" to="/patients">
+                            <i className={'small material-icons icon-sidenav ' + this.isActive('list-patient')}>list</i>
+                        </Link>
                     </Col>
                     <Col s={2}>
-                        <Link to="/patient/create"><i class="small material-icons">create</i></Link>
+                        <Link className="link-nav" to="/patient/create">
+                            <i className={'small material-icons icon-sidenav ' + this.isActive('create-patient')}>create</i>
+                        </Link>
                     </Col>
                     <Col s={2}>
-                        <Link to="/calendar"><i class="small material-icons">perm_contact_calendar</i></Link>
+                        <Link className="link-nav" to="/calendar">
+                            <i className={'small material-icons icon-sidenav ' + this.isActive('calendar')}>perm_contact_calendar</i>
+                        </Link>
                     </Col>
                     <Col s={2}>
-                        <Link to="/profile"><i class="small material-icons">person</i></Link>
+                        <Link className="link-nav" to="/profile">
+                            <i className={'small material-icons icon-sidenav ' + this.isActive('profile')}>person</i>
+                        </Link>
                     </Col>
                     <Col s={2}>
-                        <Link onClick={this.logOut} to="/signin"><i class="small material-icons">arrow_drop_down_circle</i></Link>
+                        <Link className="link-nav" onClick={this.logOut} to="/signin">
+                            <i className="small material-icons icon-sidenav">arrow_drop_down_circle</i>
+                        </Link>
                     </Col>
                 </Row>
             </div>
